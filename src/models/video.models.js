d33865b5d86f02ff9684b64d0135b3ebcf744ea2
Umar420 <umarmohammad356@gmail.com>
2026-08-30 18:39:46 +0500

    cloudinary and multer

---
+++ src/models/video.models.js
@@ -0,0 +1,53 @@
+import mongoose, {Schema} from "mongoose";
+
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({    
+    title: {
+        type: String,
+        required: true, 
+    },
+    description: {
+        type: String,
+        required: true, 
+    },  
+    videoFile: {
+        type: String, // cloudinary url
+        required: true,
+    },
+    thumbnail: {
+        type: String, // cloudinary url
+        required: true, 
+    },
+    views: {
+        type: Number,   
+        default: 0
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        
+    },
+    isPublished:{
+        type:Boolean,
+        default:false
+    },
+    duration:{
+        type:Number,
+        required:true
+    }
+},
+    {
+        timestamps:true
+    }
+)
+
+
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+
+
+const Video=mongoose.model("Video",videoSchema)
+
+export default Video;
